a5c257775ecadbf046e7033a511d7e634f57a2a9
Kacper Nowaczyk <kacpern30@gmail.com>
2023-04-05 12:44:39 +0200

    adjusted rest names

--- app/src/androidTest/java/com/kn/amproject/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kn/amproject/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.revolshen.firebaseseriesmini", appContext.packageName)
+        assertEquals("com.kn.amproject", appContext.packageName)
     }
 }
